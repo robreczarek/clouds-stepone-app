039ea85bcf0652c03fb656e798036b31416e63f1
Robert Obreczarek <robert@obreczarek.com>
2016-12-30 00:19:15 +0100

    added login form added missing fields in packages.json

--- src/Login.js
+++ src/Login.js
@@ -2,7 +2,22 @@ import React, { Component } from 'react';
 
 class Login extends React.Component {
   render() {
-    return (<div>Login</div>);
+    return (
+        <div>
+          <h3 className="section-header">
+            Login
+          </h3>
+
+          <form className="frmlogin">
+            <label>
+              Email:
+              <input type="text" name="email" />
+            </label>
+            <input type="submit" value="Log In" />
+          </form>
+        </div>
+
+    )
   }
 }
 
