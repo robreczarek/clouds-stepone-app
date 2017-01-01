69c10742a62e48a5be2b365083af2aac75a258c4
Robert Obreczarek <robert@obreczarek.com>
2017-01-01 20:17:39 +0100

    Ran npm run eject to get control over the Webpack configuration. This was necessary to add Material UI. Updated development webpack script to add workflow for Material UI and Formsy libraries Added Formsy form to logout component Removed create-react-app readme file Added install and run instructions to README.md

--- src/List.js
+++ src/List.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import Formsy from 'formsy-react';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Paper from 'material-ui/Paper';
+import RaisedButton from 'material-ui/RaisedButton';
+import MenuItem from 'material-ui/MenuItem';
+import { FormsyCheckbox, FormsyDate, FormsyRadio, FormsyRadioGroup,
+    FormsySelect, FormsyText, FormsyTime, FormsyToggle, FormsyAutoComplete } from 'formsy-material-ui/lib';
 
 class List extends Component {
 
@@ -11,12 +19,32 @@ class List extends Component {
   }
 
   render() {
+
     return (
       <div>
         <h3>List</h3>
-        <span className="mdl-badge" data-badge="4">Inbox</span>
+
+        <MuiThemeProvider muiTheme={getMuiTheme()}>
+          <Paper>
+            <Formsy.Form
+            >
+              <FormsyText
+                name="name"
+                validations="isWords"
+                required
+                hintText="What is your name?"
+                floatingLabelText="Name"
+              />
+              <RaisedButton
+                type="submit"
+                label="Submit"
+              />
+            </Formsy.Form>
+          </Paper>
+        </MuiThemeProvider>
       </div>
     );
+
   }
 }
 
--- src/Logout.js
+++ src/Logout.js
@@ -1,29 +1,60 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import Formsy from 'formsy-react';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Paper from 'material-ui/Paper';
+import RaisedButton from 'material-ui/RaisedButton';
+import MenuItem from 'material-ui/MenuItem';
+import { FormsyCheckbox, FormsyDate, FormsyRadio, FormsyRadioGroup,
+    FormsySelect, FormsyText, FormsyTime, FormsyToggle, FormsyAutoComplete } from 'formsy-material-ui/lib';
 
 import './Logout.css';
+
+injectTapEventPlugin();
 
 class Logout extends Component {
 
   constructor(props) {
     super(props);
 
+    this.state = {
+      canSubmit: false
+    };
+
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleSubmit(e) {
-    e.preventDefault();
     localStorage.email = '';
     this.props.router.replace('/login');
   }
 
+  submitForm(data) {
+    alert(JSON.stringify(data, null, 4));
+  }
+
+  notifyFormError(data) {
+    console.error('Form error:', data);
+  }
+
   render() {
+
     if (localStorage.email) {
       return (
         <div className="logoutBox">
-          <form onSubmit={this.handleSubmit}>
-            <input type="submit" value="Log out" />
-          </form>
+
+          <MuiThemeProvider muiTheme={getMuiTheme()}>
+            <Formsy.Form
+              onSubmit={this.handleSubmit}
+            >
+              <RaisedButton
+                type="submit"
+                label="Log out"
+              />
+            </Formsy.Form>
+        </MuiThemeProvider>
         </div>
       )
     } else {
